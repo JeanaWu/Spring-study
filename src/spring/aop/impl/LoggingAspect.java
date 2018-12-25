6920c10f3893711e62eab13292b870951700b772
JeanaWu <30956759+JeanaWuuu@users.noreply.github.com>
2018-12-25 17:29:24 +0800

    aspect识别不了

--- src/spring/aop/impl/LoggingAspect.java
+++ src/spring/aop/impl/LoggingAspect.java
@@ -1,14 +1,12 @@
 package spring.aop.impl;
 
-
 import org.springframework.stereotype.Component;
 
-//把这个类声明为一个切面：需要把该类放入到IOC容器中
-@Aspect
-@Component
-class LoggingAspect{
-    @before
-    public void beforeMehod(){
+public class LoggingAspect {
+
+    @Aspect
+    @Component
+    public void beforeMethod(){
         System.out.println("The method begins");
     }
 }
